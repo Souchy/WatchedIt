d0401650d3350a9cbb0c09ae257676b73286ddea
Souchy <robyn.g@hotmail.fr>
2026-01-04 17:14:10 -0500

    AuthSignin providers array

--- app/src/components/auth-module/auth-signin/AuthSignin.ts
+++ app/src/components/auth-module/auth-signin/AuthSignin.ts
@@ -1,27 +1,31 @@
 import { ILogger, inject, resolve } from "aurelia";
-import { fromState, IStore } from "@aurelia/state";
-import { AppState } from "src/core/state/AppState";
+import { IStore } from "@aurelia/state";
 import { SupabaseService } from "src/core/services/SupabaseService";
-import { UserChangedAction } from "src/core/state/actions/UserChangedAction";
-import { Session } from "@supabase/auth-js/dist/module/lib/types";
-
 
 @inject(IStore, SupabaseService)
 export class AuthSignin {
 	private readonly logger: ILogger = resolve(ILogger).scopeTo('AuthSignin');
 	private supabase: SupabaseService = resolve(SupabaseService);
 
-	public async signinWithAzure(): Promise<void> {
-		this.logger.debug('AuthSignin signinWithAzure called');
-		await this.supabase.signinWith('azure');
-	}
-	public async signinWithGoogle(): Promise<void> {
-		this.logger.debug('AuthSignin signinWithGoogle called');
-		await this.supabase.signinWith('google');
-	}
-	public async signinWithGithub(): Promise<void> {
-		this.logger.debug('AuthSignin signinWithGithub called');
-		await this.supabase.signinWith('github');
-	}
+	private readonly providers = [
+		{
+			name: 'azure',
+			displayName: 'Microsoft',
+			iconClass: 'fi fi-brands-microsoft',
+			disabled: false,
+		},
+		{
+			name: 'github',
+			displayName: 'GitHub',
+			iconClass: 'fi fi-brands-github',
+			disabled: false,
+		},
+		{
+			name: 'google',
+			displayName: 'Google',
+			iconClass: 'fi fi-brands-google',
+			disabled: true,
+		}
+	];
 
 }
